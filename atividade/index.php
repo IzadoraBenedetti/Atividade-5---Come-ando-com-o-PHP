e220a3bd972caeb2bb4b3b58f013c163814c4714
IzadoraBenedetti <160615439+IzadoraBenedetti@users.noreply.github.com>
2024-08-29 11:39:39 -0300

    Add files via upload

---
+++ atividade/index.php
@@ -0,0 +1,40 @@
+<html lang="por-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Atividade 5 </title>
+</head>
+
+<body>
+    <h1>Calcular a Área do Triângulo</h1>
+    <form method="POST" action="calcular">
+        <div>
+            <label for="base">Base do triângulo:</label>
+            <input type="number" id="base" name="base" required><br><br>
+        </div>
+        <div>
+            <label for="altura">Altura do triângulo:</label>
+            <input type="number" id="altura" name="altura" required><br><br>
+        </div>
+        <div>
+            <input type="submit" value="Calcular">
+        </div>
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $base = $_POST['base'];
+        $altura = $_POST['altura'];
+        $area = ($base * $altura) / 2;
+        $limite = 100; // Definindo o valor de $limite
+        if ($area > $limite) {
+            echo "<h1>A área do triângulo é $area, que é maior que $limite.</h1>";
+        } else {
+            echo "<h1>A área do triângulo é $area, que é menor ou igual a $limite.</h1>";
+        }
+    }
+    ?>
+</body>
+
+</html>
